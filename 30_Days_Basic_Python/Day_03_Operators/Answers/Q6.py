d4f39324648bcdf5ac9d92566ecc97fe52b3d683
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-08-28 20:42:48 +0530

    Add files via upload

---
+++ 30_Days_Basic_Python/Day_03_Operators/Answers/Q6.py
@@ -0,0 +1,9 @@
+'''
+  Get length and width of a rectangle using prompt.
+ Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+'''
+
+length = int(input('Enter length of rectangle: '))
+width = int(input('Enter width of rectangle: '))
+area = length * width
+perimeter = 2 * (length + width)
---
+++ 30_Days_Basic_Python/Day_03_Operators/Answers/Q7.py
@@ -0,0 +1,11 @@
+'''
+
+  Get radius of a circle using prompt.
+  Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14
+'''
+
+radius = float(input('Enter radius of circle: '))
+area = 3.14 * radius * radius
+circumference = 2 * 3.14 * radius
+print('The area of the circle is', area)
+print('The circumference is', circumference)
