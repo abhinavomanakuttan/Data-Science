f0f8de2f9df5f2f24ce0bf756457a9a35f2df6b6
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-08-30 08:51:14 +0530

    Add files via upload

---
+++ 30_Days_Basic_Python/Day_03_Operators/Answers/Q8.py
@@ -0,0 +1,7 @@
+'''
+Calculate the slope, x-intercept and y-intercept of y = 2x -2
+'''
+
+x = 2
+y = 2 * x - 2
+print(y)
---
+++ 30_Days_Basic_Python/Day_03_Operators/Answers/Q9.py
@@ -0,0 +1,13 @@
+'''
+Slope is (m = y2-y1/x2-x1).
+Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+'''
+
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+m = (y2-y1)/(x2-x1)
+print("Slope is : ",m)
