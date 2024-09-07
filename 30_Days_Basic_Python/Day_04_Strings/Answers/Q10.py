021445b3b293e0314a41b8e9a42122caf1400ac9
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-09-07 12:05:24 +0530

    Add files via upload

---
+++ 30_Days_Basic_Python/Day_04_Strings/Answers/Q10.py
@@ -0,0 +1,9 @@
+'''
+Check if Coding For All string contains a word Coding using the method index, find or other methods
+'''
+
+string = "Coding For All"
+if "Coding" in string:
+    print("The word 'Coding' is in the string.")
+else:
+    print("The word 'Coding' is not in the string.")
