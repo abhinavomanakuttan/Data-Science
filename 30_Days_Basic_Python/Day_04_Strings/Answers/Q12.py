9a2db47656622c99dccf67ebd8af72f21a75204e
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-09-07 12:08:01 +0530

    Add files via upload

---
+++ 30_Days_Basic_Python/Day_04_Strings/Answers/Q12.py
@@ -0,0 +1,9 @@
+'''
+Change Python for Everyone to Python for All using the replace method or other methods.
+'''
+import re
+
+str1 = "Python for Everyone"
+
+x = re.sub("Everyone","All",str1)
+print(x)
---
+++ 30_Days_Basic_Python/Day_04_Strings/Answers/Q14.py
@@ -0,0 +1,8 @@
+'''
+"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+'''
+
+str1 = "Facebook, Google, Microsoft,Apple, IBM, Oracle, Amazon"
+
+words = str1.split(",")
+print(words)
