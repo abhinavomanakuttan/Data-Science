9f1843f0c4d2cb13b23e43b3bd397bdec0b59cc4
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-09-08 20:44:42 +0530

    Delete 30_Days_Basic_Python/Day_04_Strings/Answers/Q10.py

--- 30_Days_Basic_Python/Day_04_Strings/Answers/Q10.py
+++
@@ -1,9 +0,0 @@
-'''
-Check if Coding For All string contains a word Coding using the method index, find or other methods
-'''
-
-string = "Coding For All"
-if "Coding" in string:
-    print("The word 'Coding' is in the string.")
-else:
-    print("The word 'Coding' is not in the string.")
