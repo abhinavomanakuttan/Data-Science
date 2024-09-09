e5c3fdaec7ae8194c61aa241fe7dfc2dbba6b9ca
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-09-09 10:08:52 +0530

    Delete 30_Days_Basic_Python/Day_04_Strings/Answers/Q14.py

--- 30_Days_Basic_Python/Day_04_Strings/Answers/Q14.py
+++
@@ -1,8 +0,0 @@
-'''
-"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
-'''
-
-str1 = "Facebook, Google, Microsoft,Apple, IBM, Oracle, Amazon"
-
-words = str1.split(",")
-print(words)
