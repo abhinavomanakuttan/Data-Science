b763e1fab395cad154fcb89d44ee4acd1cdfaa64
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-09-11 02:34:00 +0530

    Delete 30_Days_Basic_Python/Day_04_Strings/Answers/Q20.py

--- 30_Days_Basic_Python/Day_04_Strings/Answers/Q20.py
+++
@@ -1,8 +0,0 @@
-'''
-Use index to determine the position of the first occurrence of C in Coding For All.
-'''
-
-txt = "Coding For All"
-postion =txt.index("C")
-# To find the index of a specific letter -> variable_name.index("letter ")
-print(postion)
