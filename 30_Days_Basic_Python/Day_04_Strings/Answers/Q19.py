834408be400716384e21630b68445795629d0ecc
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-09-11 02:33:44 +0530

    Delete 30_Days_Basic_Python/Day_04_Strings/Answers/Q19.py

--- 30_Days_Basic_Python/Day_04_Strings/Answers/Q19.py
+++
@@ -1,8 +0,0 @@
-'''
-Create an acronym or an abbreviation for the name 'Coding For All'.
-'''
-
-phrase = "Coding For All"
-words = phrase.split()  # Split the phrase into words
-acronym = ''.join(word[0].upper() for word in words)  # Take the first letter of each word and capitalize it
-print(acronym)
